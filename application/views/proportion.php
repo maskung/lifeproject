e678e654c41195882af20e4726e3a77af9b66ac1
maskung <suphanut@gmail.com>
2016-04-22 14:52:48 +0700

    remove some chart

--- application/views/proportion.php
+++ application/views/proportion.php
@@ -42,22 +42,6 @@
                     <!-- /.panel -->
                 </div>
                 <!-- /.col-lg-6 -->
-                <div class="col-lg-6">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            Bar Chart Example
-                        </div>
-                        <!-- /.panel-heading -->
-                        <div class="panel-body">
-                            <div class="flot-chart">
-                                <div class="flot-chart-content" id="flot-bar-chart"></div>
-                            </div>
-                        </div>
-                        <!-- /.panel-body -->
-                    </div>
-                    <!-- /.panel -->
-                </div>
-                <!-- /.col-lg-6 -->
             </div>
             <!-- /.row -->
         </div>
@@ -114,48 +98,6 @@ $(function() {
 
 });
 
-//Flot Bar Chart
-
-$(function() {
-
-    var barOptions = {
-        series: {
-            bars: {
-                show: true,
-                barWidth: 43200000
-            }
-        },
-        xaxis: {
-            mode: "time",
-            timeformat: "%m/%d",
-            minTickSize: [1, "day"]
-        },
-        grid: {
-            hoverable: true
-        },
-        legend: {
-            show: false
-        },
-        tooltip: true,
-        tooltipOpts: {
-            content: "x: %x, y: %y"
-        }
-    };
-    var barData = {
-        label: "bar",
-        data: [
-            [1, 1000],
-            [2, 2000],
-            [3, 3000],
-            [4, 4000],
-            [5, 5000],
-            [6, 6000]
-        ]
-    };
-    $.plot($("#flot-bar-chart"), [barData], barOptions);
-
-});
-
     </script>
 
     <script>
@@ -166,7 +108,7 @@ $(function() {
             type: 'column'
         },
         title: {
-            text: 'กราฟแสดงจำวนผู้ลงทะเบียนเรียนในแต่ละวิชาในค่าย  Life Sharing'
+            text: 'กราฟแสดงจำนวนผู้ลงทะเบียนเรียนในแต่ละวิชาในค่าย  Life Sharing'
         },
         subtitle: {
             text: 'Source: <a href="http://www.churchofcovenant.com/">COC</a>'
